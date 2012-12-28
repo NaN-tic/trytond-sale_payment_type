9b50ecc65c9288d939d8676aa501dcea2b5b5965
resteve <resteve@zikzakmedia.com>
2012-12-28 13:53:38 +0100

    Package name trytonspain + download url

--- setup.py
+++ setup.py
@@ -27,13 +27,13 @@ for dep in info.get('depends', []):
 requires.append('trytond >= %s.%s, < %s.%s' %
         (major_version, minor_version, major_version, minor_version + 1))
 
-setup(name='trytonzz_sale_payment_type',
+setup(name='trytonspain_sale_payment_type',
     version=info.get('version', '0.0.1'),
     description='Tryton module for Sale Payment Type',
     author='Zikzakmedia SL',
     author_email='zikzak@zikzakmedia.com',
     url='http://www.zikzakmedia.com',
-    download_url="https://bitbucket.org/zikzakmedia/trytond-sale_payment_type",
+    download_url="https://bitbucket.org/trytonspain/trytond-sale_payment_type",
     package_dir={'trytond.modules.sale_payment_type': '.'},
     packages=[
         'trytond.modules.sale_payment_type',

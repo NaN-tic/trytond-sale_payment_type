6d351618313cdd3fa726a21b4052ab2e579ba80a
jmartin <jmartin@zikzakmedia.com>
2013-05-06 12:08:27 +0200

    pep8 adapted

--- sale.py
+++ sale.py
@@ -1,5 +1,5 @@
 #This file is part of sale_payment_type module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 
 from trytond.model import fields
@@ -13,6 +13,7 @@ _STATES = {
     'readonly': Eval('state') != 'draft',
 }
 _DEPENDS = ['state']
+
 
 class Sale:
     'Sale'

38c17620f3cb06524e08ceb365faa31dccddd340
resteve <resteve@zikzakmedia.com>
2015-12-01 11:20:54 +0100

    Tests

--- tests/test_sale_payment_type.py
+++ tests/test_sale_payment_type.py
@@ -1,26 +1,15 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-# This file is part of sale_payment_type module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the sale_payment_type module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SalePaymentTypeTestCase(unittest.TestCase):
-    'Test SalePaymentType module'
+class SalePaymentTypeTestCase(ModuleTestCase):
+    'Test Sale Payment Type module'
+    module = 'sale_payment_type'
 
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_payment_type')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_payment_type')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
 
 def suite():
     suite = trytond.tests.test_tryton.suite()

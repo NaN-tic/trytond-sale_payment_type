ea084ed44367b972d019b3db3bda5447cd6fe076
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-10-03 11:47:14 +0200

    Correctly define test dependencies
    proteus dependency for test where wrongly defined on devependency_links
    attribute instead of on test_requires.
    Introduced on changeset 3a6ca12c36aa

--- setup.py
+++ setup.py
@@ -52,8 +52,8 @@ for dep in info.get('depends', []):
         requires.append(get_require_version('%s_%s' % (prefix, dep)))
 requires.append(get_require_version('trytond'))
 
-tests_require = []
-dependency_links = [get_require_version('proteus')]
+tests_require = [get_require_version('proteus')]
+dependency_links = []
 if minor_version % 2:
     # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')

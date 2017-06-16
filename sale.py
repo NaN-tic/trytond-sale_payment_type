62b41773176383a2371d054c05940c8c47bad3a2
Raimon Esteve <raimon@nan-tic.com>
2017-06-16 12:28:41 +0200

    Set payment type receivable/payable according to untaxed amount

--- sale.py
+++ sale.py
@@ -1,7 +1,7 @@
-# This file is part of sale_payment_type module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-
+#This file is part of sale_payment_type module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
+from decimal import Decimal
 from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
@@ -12,6 +12,7 @@ _STATES = {
     'readonly': Eval('state') != 'draft',
 }
 _DEPENDS = ['state']
+ZERO = Decimal('0.0')
 
 
 class PaymentType:
@@ -28,10 +29,7 @@ class Sale:
     __metaclass__ = PoolMeta
     __name__ = 'sale.sale'
     payment_type = fields.Many2One('account.payment.type', 'Payment Type',
-        states=_STATES, depends=_DEPENDS,
-        domain=[
-            ('kind', 'in', ['both', 'receivable']),
-            ])
+        domain=[('kind', '=', 'receivable')], states=_STATES, depends=_DEPENDS)
 
     @classmethod
     def default_payment_type(cls):
@@ -49,23 +47,65 @@ class Sale:
 
     def _get_grouped_invoice_domain(self, invoice):
         invoice_domain = super(Sale, self)._get_grouped_invoice_domain(invoice)
+
+        # know about the invoice is payable or receivable payment type (untaxed amount)
+        # _get_grouped_invoice_domain not return an invoice with lines and untaxed
+        # amount; we need to recompute those values
+        if not hasattr(invoice, 'untaxed_amount'):
+            has_lines = hasattr(invoice, 'lines')
+            if not has_lines:
+                invoice_lines = []
+                for line in self.lines:
+                    if line.type == 'line':
+                        iline, = line.get_invoice_line()
+                        setattr(iline, 'amount', iline.on_change_with_amount())
+                        invoice_lines.append(iline)
+                invoice.lines = invoice_lines
+            invoice.on_change_lines()
+            # reset lines
+            if not has_lines:
+                invoice.lines = []
+
         invoice_domain.append(
             ('payment_type', '=', self._get_invoice_payment_type(invoice)))
         return invoice_domain
 
     def _get_invoice_sale(self):
         invoice = super(Sale, self)._get_invoice_sale()
-        invoice.payment_type = self._get_invoice_payment_type(invoice)
+        if self.payment_type:
+            # set None payment type to control payable/receivable kind
+            # depend untaxed amount
+            invoice.payment_type = None
+        return invoice
+
+    def create_invoice(self):
+        invoice = super(Sale, self).create_invoice()
+
+        if invoice:
+            payment_type = self._get_invoice_payment_type(invoice)
+            if payment_type:
+                invoice.payment_type = payment_type
+                invoice.save()
         return invoice
 
     def _get_invoice_payment_type(self, invoice):
-        if getattr(invoice, 'payment_type', False):
-            return invoice.payment_type
-        if self.payment_type:
-            if self.payment_type.kind != 'both' and self.total_amount < 0.0:
-                return self.party.supplier_payment_type
-            else:
-                return self.payment_type
+        if not self.payment_type:
+            return None
+
+        if invoice.untaxed_amount >= ZERO:
+            kind = 'receivable'
+            name = 'customer_payment_type'
+        else:
+            kind = 'payable'
+            name = 'supplier_payment_type'
+
+        if self.payment_type.kind == kind:
+            return self.payment_type
+        else:
+            payment_type = getattr(self.party, name)
+            if payment_type:
+                return payment_type
+        return None
 
 
 class Opportunity:
@@ -77,3 +117,4 @@ class Opportunity:
         if sale.party and sale.party.customer_payment_type:
             sale.payment_type = self.party.customer_payment_type
         return sale
+
